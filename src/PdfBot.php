e6cb157bc7a4f1543f4fce13705ffe359d8b82cc
Esben Petersen <esbenspetersen@gmail.com>
2017-08-13 17:26:20 +0200

    add api token

--- src/PdfBot.php
+++ src/PdfBot.php
@@ -15,8 +15,15 @@ class PdfBot
 
     public function push($url, array $meta = [])
     {
+        $headers = [];
+
+        if (!empty($this->config['api_token'])) {
+            $headers['authorization'] = 'Bearer ' . $this->config['api_token'];
+        }
+
         $client = new Client();
         return $client->request('POST', $this->config['server'], [
+            'headers' => $headers,
             'json' => [
                 'url' => $url,
                 'meta' => (object) $meta
--- src/config.php
+++ src/config.php
@@ -1,9 +1,11 @@
 <?php
 
 return [
+    'api_token' => env('PDF_BOT_API_TOKEN', ''),
+
     'secret' => env('PDF_BOT_SECRET', ''),
 
-    'server' => 'http://localhost:3000',
+    'server' => env('PDF_BOT_SERVER', 'http://localhost:3000'),
 
     'header-namespace' => 'X-PDF-'
 ];
