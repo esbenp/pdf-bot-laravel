0fc91ff3dca842959a308674bd1c3666f861dc3d
Esben Petersen <esbenspetersen@gmail.com>
2019-08-25 11:15:00 +0200

    add support for priority

--- src/PdfBot.php
+++ src/PdfBot.php
@@ -13,7 +13,7 @@ class PdfBot
         $this->config = $config;
     }
 
-    public function push($url, array $meta = [])
+    public function push($url, array $meta = [], $priority = null)
     {
         $headers = [];
 
@@ -21,13 +21,19 @@ class PdfBot
             $headers['authorization'] = 'Bearer ' . $this->config['api_token'];
         }
 
+        $body = [
+            'url' => $url,
+            'meta' => (object) $meta
+        ];
+
+        if (is_integer($priority)) {
+            $body['priority'] = $priority;
+        }
+
         $client = new Client();
         return $client->request('POST', $this->config['server'], [
             'headers' => $headers,
-            'json' => [
-                'url' => $url,
-                'meta' => (object) $meta
-            ]
+            'json' => $body
         ]);
     }
 }
